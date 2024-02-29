ac9b2bc58f0d731249d304cd8bcb6257ea5723e7
GyozoCliq <g.karer@cliqdigital.com>
2024-02-29 18:17:48 +0100

    Initial version of test plus the application YAML is added

---
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,40 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CatalogServiceApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void whenPostRequest_ThenBookCreated() {
+		//GIVEN
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+		//WHEN
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				//THEN
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,49 @@
+package com.polarbookshop.catalogservice.domain;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookValidationTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void whenAllFieldsCorrectThenSucceed() {
+        //GIVEN
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+
+        //WHEN
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+
+        //THEN
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    public void whenIsbnDefinedButIncorrect_ThenFail() {
+        //GIVEN
+        var book = new Book("a23456789", "Title", "Author", 9.90);
+
+        //WHEN
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+
+        //THEN
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The ISBN format must be valid.");
+    }
+}
